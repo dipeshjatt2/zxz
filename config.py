bde3862c9bd1b5f395944eaa4a9f03f343342e7d
Kaminanoone <oberoielvish84@gmail.com>
2024-08-29 11:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6998421847:AAFY9AdMoBRkDZpAgKzOnxOXdp2NymYKVDM")
-    API_ID = int(os.environ["API_ID", 20346550]
-    API_HASH = os.environ["API_HASH", "bc79c3bea7a626887bdc0871eecf0327"]
-    AUTH_USERS = "5665231556"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7252938527:AAGkslGBnXthvmRsP6N0V8CZW0xWQ2WzhMc")
+    API_ID = int(os.environ["API_ID", 29640188]
+    API_HASH = os.environ["API_HASH", "e470abc84a3bc445997ee4ea5be87deb"]
+    AUTH_USERS = "7504263874,6886826162 """
 # jisko bhi is repo ko deploy karne me error aa raha ho ( https://t.me/Stubborn1223 ) ye mere TG ka id ha message karke iska screen shot send kar dena me error solve kar dunga. 
